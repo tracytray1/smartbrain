3440ed220f6dc1369aeebe1a25176446ca886f58
Tracy Kabuya <tracykabuya@gmail.com>
2025-06-06 00:44:26 +0200

    update : added port to package jsonl

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 10000
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 10000
+  }
 })
