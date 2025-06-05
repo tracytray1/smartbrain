fcff3994a03b462ab75f4d83b486b59e146b0c76
Tracy Kabuya <tracykabuya@gmail.com>
2025-06-06 01:30:21 +0200

    update: nouvelle tentative push

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ function App() {
 
   const onButtonSubmit = () => {
     setImageUrl(input);
-      fetch('http://localhost:3000/imageurl', {
+    fetch('https://smartbrain-backend-n3j8.onrender.com/imageurl', {
         method: 'post',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
@@ -74,7 +74,7 @@ function App() {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://smartbrain-backend-n3j8.onrender.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
